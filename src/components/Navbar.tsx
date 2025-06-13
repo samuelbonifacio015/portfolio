00dc3ca8a4d8b0c0d09f8622197923533fe8383a
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2025-06-12 20:38:49 -0500

    minor fixes (12/06)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex items-center justify-between">
           <a href="#home" className="text-xl font-display font-bold tracking-tight text-white">
-            <span className="text-primary">Port</span>folio
+            <span className="text-primary">samuel</span>.dev
           </a>
 
           {/* Desktop Navigation */}
